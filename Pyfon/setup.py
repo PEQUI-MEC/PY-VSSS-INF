a3b3b02077e1a88fc685730075497ba461bf3ec6
Rafael Falcão Gomes Jardim <rafaelfgjardim@gmail.com>
2018-08-21 17:53:28 -0300

    Initial project skeleton
    Esqueleto, do inglês, skeleton.

---
+++ Pyfon/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+config = {
+    'description': 'Pyfon Project',
+    'author': 'Pyfon Inf',
+    'url': 'https://github.com/PEQUI-MEC/PY-VSSS-INF',
+    'download_url': 'https://github.com/PEQUI-MEC/PY-VSSS-INF/archive/master.zip',
+    'author_email': 'emaildopequi@pequi.com.br',
+    'version': '0.0.1',
+    'install_requires': [],
+    'packages': ['NAME'],
+    'scripts': [],
+    'name': 'Pyfon'
+}
+
+#build steps for cython
+
+setup(**config)
