7818d82e6dd37bdff5bb08f9b7d57c2a760a0ec3
Rafael Falcão Gomes Jardim <rafaelfgjardim@gmail.com>
2018-08-25 20:29:37 -0300

    Primeiros testes da main

---
+++ Pyfon/hades.py
@@ -0,0 +1,23 @@
+import numpy as np
+import cv2
+
+
+class Hades:
+
+    def riseUpCapture():
+        return True
+
+    def riseUpCommunication():
+        return True
+
+    def puppetLoop():
+        return True
+
+    def updatePositions():
+        return True
+
+    def updateFormation():
+        return True
+
+    def createFormation():
+        return True
