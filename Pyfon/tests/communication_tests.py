b0890f22784752e770c8eb11c6e1953e7981b69f
Rafael Alessandro <noprafa@gmail.com>
2018-08-25 17:44:52 -0300

    Testes iniciais da comunicação
    hehehe

--- Pyfon/tests/communication_tests.py
+++ Pyfon/tests/communication_tests.py
@@ -1,36 +1,42 @@
 import unittest
-import basic_operations
+import sys
+sys.path.append("../")
+import communication.hermes
 
 class CommunicationTest(unittest.TestCase):
-
-	def test_add(self):
-		self.assertEqual(basic_operations.add(-1, 1), 0)
-		self.assertEqual(basic_operations.add(4, 4), 8)
-		self.assertEqual(basic_operations.add(-3, 2), -1)
-		self.assertEqual(basic_operations.add(1, 7), 8)
-		self.assertEqual(basic_operations.add(6, 3), 9)
-
+	#unicast
+	def test_sendMessages(self):
+		self.assertEqual(hermes.sendMessages(1, "0.7;0.6"), true)
+		self.assertEqual(hermes.sendMessages(2, "0.7;0.6"), true)
+		self.assertEqual(hermes.sendMessages(3, "0.8;0.1"), true)
+		self.assertEqual(hermes.sendMessages(4, "0.4, 0.2"), false)
+	#multicast
+	def test_sendMessages(self):
+		self.assertEqual(hermes.sendMessages(1, "INF@R1@0.7;0.3"), true)
+		self.assertEqual(hermes.sendMessages(2, "INF@R2@0.7;0.3"), true)
+		self.assertEqual(hermes.sendMessages(3, "INF@R3@0.7;0.3"), true)
+		self.assertEqual(hermes.sendMessages(4, "INF@R4@0.7;0.3"), false)
 
 	def test_subtract(self):
-		self.assertEqual(basic_operations.subtract(-1, 1), -2)
-		self.assertEqual(basic_operations.subtract(4, 4), 0)
-		self.assertEqual(basic_operations.subtract(-3, 2), -5)
-		self.assertEqual(basic_operations.subtract(1, 7), -6)
-		self.assertEqual(basic_operations.subtract(6, 3), 3)
+		self.assertEqual(hermes.subtract(-1, 1), -2)
+		self.assertEqual(hermes.subtract(4, 4), 0)
+		self.assertEqual(hermes.subtract(-3, 2), -5)
+		self.assertEqual(hermes.subtract(1, 7), -6)
+		self.assertEqual(hermes.subtract(6, 3), 3)
 
 	def test_multiply(self):
-		self.assertEqual(basic_operations.multiply(-1, 1), -1)
-		self.assertEqual(basic_operations.multiply(4, 4), 16)
-		self.assertEqual(basic_operations.multiply(-3, 2), -6)
-		self.assertEqual(basic_operations.multiply(1, 7), 7)
-		self.assertEqual(basic_operations.multiply(6, 3), 18)
+		self.assertEqual(hermes.multiply(-1, 1), -1)
+		self.assertEqual(hermes.multiply(4, 4), 16)
+		self.assertEqual(hermes.multiply(-3, 2), -6)
+		self.assertEqual(hermes.multiply(1, 7), 7)
+		self.assertEqual(hermes.multiply(6, 3), 18)
 
 	def test_divide(self):
-		self.assertEqual(basic_operations.divide(-1, 1), -1)
-		self.assertEqual(basic_operations.divide(4, 4), 1)
-		self.assertEqual(basic_operations.divide(-3, 2), -1.5)
-		self.assertEqual(basic_operations.divide(6, 2), 3)
-		self.assertEqual(basic_operations.divide(6, 3), 2)
+		self.assertEqual(hermes.divide(-1, 1), -1)
+		self.assertEqual(hermes.divide(4, 4), 1)
+		self.assertEqual(hermes.divide(-3, 2), -1.5)
+		self.assertEqual(hermes.divide(6, 2), 3)
+		self.assertEqual(hermes.divide(6, 3), 2)
 '''
 	def test_pow(self):
 		result = basic_operations.pow(2, 3)
