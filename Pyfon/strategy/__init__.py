fa5bc80217eee82ee51fa0f0b5877b6a87c47e1b
Bryan Lincoln <bryanufg@gmail.com>
2018-08-26 23:28:09 -0300

    testes iniciais - ainda não dá pra usar o unittest discover

---
+++ Pyfon/strategy/__init__.py
@@ -0,0 +1,24 @@
+class Athena:
+    def setup(self, width, height, callback):
+        return True
+
+    def getTargets(self, positions):
+        pass
+
+    def interpretJSON(self, positions):
+        pass
+
+    def generateResponse(self):
+        pass
+
+    def analyzeState(self):
+        pass
+
+    def setRoles(self):
+        pass
+
+    def selectTactics(self):
+        pass
+
+    def selectActions(self):
+        pass
