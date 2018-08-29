9bccb254a7183d4dba401cba8c6a40e068078856
luanagbmartins <luanagbmartins@gmail.com>
2018-08-29 20:13:34 -0300

    Implementação do módulo de ações(forma básica). Faltando algumas modificações

--- Pyfon/control/actions.py
+++ Pyfon/control/actions.py
@@ -1,10 +1,11 @@
 from . import constants
+import math
 
 
 class Actions:
     robot = []
 
-    def setup(robot):
+    def setup(self, robot):
         __class__.robot = robot
 
         if __class__.robot[constants._actionsCommand] == 'stop':
@@ -21,16 +22,30 @@ class Actions:
             return False
 
     def stop(self):
-        pass
-
-    def kick(self):
-        pass
+        __class__.robot[constants._cmdType] = 'SPEED'
+        __class__.robot[constants._vMax] = 0
+        __class__.robot[constants._vLeft] = 0
+        __class__.robot[constants._vRight] = 0
+        __class__.robot[constants._target] = [-1, -1]
 
     def lookAt(self):
-        return True
+        x = __class__.robot[constants._target][0] - __class__.robot[constants._position][0]
+        y = __class__.robot[constants._target][1] - __class__.robot[constants._position][1]
+        __class__.robot[constants._cmdType] = 'ORIENTATION'
+        __class__.robot[constants._targetOrientation] = math.atan2(x, -y)
+
+    def kick(self):
+        x = __class__.robot[constants._target][0] - __class__.robot[constants._position][0]
+        y = __class__.robot[constants._target][1] - __class__.robot[constants._position][1]
+        __class__.robot[constants._cmdType] = 'VECTOR'
+        __class__.robot[constants._transAngle] = -math.atan2(x, y)
 
     def spinClockwise(self):
-        pass
+        __class__.robot[constants._cmdType] = 'SPEED'
+        __class__.robot[constants._vLeft] = __class__.robot[constants._vMax]
+        __class__.robot[constants._vRight] = -(__class__.robot[constants._vMax])
 
     def spinCounterClockWise(self):
-        pass
+        __class__.robot[constants._cmdType] = 'SPEED'
+        __class__.robot[constants._vLeft] = -(__class__.robot[constants._vMax])
+        __class__.robot[constants._vRight] = __class__.robot[constants._vMax]
