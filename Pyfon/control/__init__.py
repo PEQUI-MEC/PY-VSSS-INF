2b8a50b8c96b3f1bfbfe1cd809cd1cdd8fa835aa
Luana Martins <luanagbmartins@gmail.com>
2018-08-29 22:16:12 -0300

    Control tests (#3)
    * testes iniciais
    
    * incrementando testes
    
    * modificação na importação (reclamações da ide)
    
    * Modificando pastas
    
    - 'tests' para 'test'
    - 'control' para 'testControl'
    
    * Atualizado .gitignore
    
    Descomentadas as linhas que não ignoravam arquivos de ambiente. Estavam gerando alterações demais no merge.

---
+++ Pyfon/control/__init__.py
@@ -0,0 +1,18 @@
+from .actions import Actions
+from .translate import Translate
+
+
+class Zeus:
+    robots = []
+
+    def setup(self):
+        return True
+
+    def getRobots(self, robots):
+        pass
+
+    def generateJson(self):
+        pass
+
+    def controlRoutine(self):
+        pass
