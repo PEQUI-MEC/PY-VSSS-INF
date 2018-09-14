336e5631e68650577de28f2bbb87d8ac25912230
Luiz Mattos <luizgustavomattoswow@gmail.com>
2018-09-14 15:31:06 -0300

    Atualizados os metodos de threshold

--- Pyfon/vision/Apolo.py
+++ Pyfon/vision/Apolo.py
@@ -8,65 +8,73 @@ from vision import Camera
 WIDTH = 640
 HEIGHT = 480
 
+#Fica definido que tudo relacionado a tag principal estara na posicao 0
+#Tudo relacionado a bola estara na posição 1
+#Tudo relacionado a tag dos adversarios estara na posicao 2
+#Tudo relacionado as tags secundarias estara na posicao 3
+#O threshold quando for setado deve estar no formato ((Hmin,HMax),(Smin,SMax),(Vmin,VMax))
 class Apolo:
 	def __init__(self):
-		self.camera = Camera.Ciclope()
+		self.ciclope = Camera.Ciclope()
 		
+		self.threshList = [None] * 4
+		self.thresholdedImages = [None] * 4
 		#Por default seta esses valores, deve ser modificado quando der o quickSave
-		self.setHueThresh(120,250)
-		self.setSaturationThresh(0,250)
-		self.setValueThresh(0,250)
-		
-	def setHueThresh(self,hMin,hMax):
-		self.hThresh = (hMin,hMax)
-		
-	def setSaturationThresh(self,sMin,sMax):
-		self.sThresh = (sMin,sMax)	
-		
-	def setValueThresh(self,vMin,vMax):
-		self.vThresh = (vMin,vMax)
-		
-	def getThreshValues(self):
-		return (self.hThresh,self.sThresh,self.vThresh)
+		self.setHSVThreshMain(((120,250),(0,250),(0,250)))
+		self.setHSVThreshBall(((120,250),(0,250),(0,250)))
+		self.setHSVThreshAdv(((120,250),(0,250),(0,250)))
+		self.setHSVThreshSecondary(((0,250),(0,250),(0,250)))
 		
 		
-	#Funçao principal da visao
-	def run(self):
-		if self.camera.isOpened():
-			#Colocar condição de parada
-			ret, frame = self.camera.getFrame()
+	#hsvThresh deve ser do tipo (hmin,hmax),(smin,smax),(vmin,vmax)
+	'''
+	Quando definir o enum, vai utilizar so essa funcao
+	def setHSVThresh(self, hsvThresh, keyword):
+		self.theshList[keyword] = hsvThresh
+	'''
+	def setHSVThreshMain(self, hsvThresh):
+		self.threshList[0] = hsvThresh
+		
+	def setHSVThreshBall(self, hsvThresh):
+		self.threshList[1] = hsvThresh
+		
+	def setHSVThreshAdv(self, hsvThresh):
+		self.threshList[2] = hsvThresh
+		
+	def setHSVThreshSecondary(self, hsvThresh):
+		self.threshList[3] = hsvThresh
+		
+	'''
+	Quando definir o enum, vai utilizar so essa função
+	def getHSVThresh(self, keyword):
+		return self.theshList[keyword]	
+	'''	
+	def getHSVThresh(self,keyword):
+		return self.threshList[keyword]
+	
+	def getThreshMain(self):
+		return self.threshList[0]
+		
+	def getThreshBall(self):
+		return self.threshList[1]
+		
+	def getThreshAdv(self):
+		return self.threshList[2]
+	
+	def getThreshSecondary(self):
+		return self.threshList[3]
+		
+		
+	def getFrame(self):
+		frame = None
+		if (self.ciclope.isCameraOpened()):
+			_, frame = self.ciclope.getFrame()
 			
-			frameHSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV);
-				
-			'''
-			Assinatura -> applyThreshold(frameHSV, HMin, HMax, SMin, SMax, VMin, VMax)
-			Retorno -> imagem binarizada de acordo com os valores de HSV passados
-			Pegar os valores de HSV da interface
-			'''
-			#Adicionar threads para fazerem o threshold da bola, tags principais e tags secundarias
-			threshFrame = self.applyThreshold(frameHSV)
-			
-			'''
-			Assinatura -> applyThreshold(thresholdedFrame, areaMinimaDaTag)
-			Retorno -> lista com as posições em x e y dos nossos robos  
-			Pegar areaMinima da interface
-			'''
-			
-			self.seeThroughMyEyes("Original",frame)
-			self.seeThroughMyEyes("Thresh",threshFrame)
-			
-			#Seta os dados dos robos e da bola
-			
-			robotList = self.findRobots(threshFrame,30)
-			#ball = self.findBall(ballThreshFrame,30)
-			robotAdvList = robotList
-			
-			#Modela os dados para o formato que a Athena recebe e retorna
-			return self.returnData(robotList,robotAdvList,(300,300))
-
-		else: 
-			print ("Nao há câmeras ou o dispositivo está ocupado")
-			return None	
+		return frame
+		
+	def getHSVFrame(self, rawFrame):
+		frameHSV = cv2.cvtColor(rawFrame, cv2.COLOR_BGR2HSV);
+		return frameHSV
 		
 	def returnData(self, robotList, robotAdvList,ball):
 		output = [
@@ -105,11 +113,23 @@ class Apolo:
 		
 		return output
 	
-	
-	def applyThreshold(self, src):
-		threshMin = (self.hThresh[0], self.sThresh[0], self.vThresh[0])
-		threshMax = (self.hThresh[1],self.sThresh[1],self.vThresh[1])
+	def applyThreshold(self,src,keyword):
+		thresh = self.getHSVThresh(keyword)
+		if keyword == 3: print ("THRESH: ", thresh)
 		
+		threshMin = (thresh[0][0], thresh[1][0], thresh[2][0])
+		threshMax = (thresh[0][1],thresh[1][1],thresh[2][1])
+
+		maskHSV = cv2.inRange(src,threshMin, threshMax)
+				
+		return maskHSV
+	
+	def applyThresholdMain(self, src):
+		thresh = self.getThreshMain()
+		
+		threshMin = (thresh[0][0], thresh[1][0], thresh[2][0])
+		threshMax = (thresh[0][1],thresh[1][1],thresh[2][1])
+
 		maskHSV = cv2.inRange(src,threshMin, threshMax)
 				
 		return maskHSV
@@ -180,3 +200,33 @@ class Apolo:
 		
 	def findBallOrientation(self,previousBallPosition, currentBallPosition):
 		pass
+		
+	#Funçao principal da visao
+	def run(self):
+		while True:
+			#Pega o frame
+			frame = self.getFrame()
+			
+			if frame is None:
+				print ("Nao há câmeras ou o dispositivo está ocupado")
+				return None
+				
+			#Transforma de BRG para HSV
+			frameHSV = self.getHSVFrame(frame)
+				
+			#Aplica todos os thresholds (pode adicionar threads)
+			for i in range(0,4,1):
+				self.thresholdedImages[i] = self.applyThreshold(frameHSV, i)
+			
+			
+			self.seeThroughMyEyes("Original",frame)
+			self.seeThroughMyEyes("Main",self.thresholdedImages[0])
+			
+			#Procura os robos
+			robotList = self.findRobots(self.thresholdedImages[0],30)
+			
+			#ball = self.findBall(ballThreshFrame,30)
+			#robotAdvList = robotList
+			
+			#Modela os dados para o formato que a Athena recebe e retorna
+			#return self.returnData(robotList,robotAdvList,(300,300))
