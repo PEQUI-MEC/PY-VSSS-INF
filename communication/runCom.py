090b4b30165e27b843ae7ca8a0824738afd295ca
Rafael Alessandro <noprafa@gmail.com>
2018-10-12 11:58:10 -0300

    Re-inserindo arquivos da comunicação com a documentação resolvida

--- communication/runCom.py
+++ communication/runCom.py
@@ -6,13 +6,13 @@ hermes = Hermes("/dev/ttyUSB0")
 
 for x in range(0,9):
 
-    if x%2==0:
-        message = "0."+str(x) +";"+ "-0." + str(x)
-    else:
-        message = "-0."+str(x) +";"+ "-0." + str(x)
-    hermes.sendMessage(1, message)
-    hermes.sendMessage(2, message)
-    hermes.sendMessage(3, message)
-    time.sleep(0.1)
+	if x%2==0:
+		message = "0."+str(x) +";"+ "-0." + str(x)
+	else:
+		message = "-0."+str(x) +";"+ "-0." + str(x) 
+	hermes.sendMessage(1, message)
+	hermes.sendMessage(2, message)
+	hermes.sendMessage(3, message)
+	time.sleep(0.1)
 
 hermes.killBee()
