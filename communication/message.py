8b862da643b91c1991c0930e6ad1f12a849d786a
Rafael Alessandro <noprafa@gmail.com>
2018-10-12 12:01:48 -0300

    Re-inserindo arquivos da comunicação com a documentação resolvida (#20)

---
+++ communication/message.py
@@ -0,0 +1,15 @@
+class Message():
+    def __init__(self, robotId, message):
+    """Message Constructor
+        
+        Store message and robot id
+        
+        Args:
+            robotId (int): Robot id
+            message (string): message to send
+
+        Returns:
+
+    """
+        self.robotId = robotId
+        self.message = message
---
+++ communication/robot.py
@@ -0,0 +1,15 @@
+class Robot():
+    def __init__(self):
+	"""Robot Constructor
+		
+		Creates a vector with robot's addresses.
+		Addresses will be used to connect with robots
+        Args:
+        
+        Returns:
+
+    """
+		self.robots = []
+		self.robots.append("\x56\x0D")
+		self.robots.append("\x6B\x0D")
+		self.robots.append("\x21\x5C")
