ee8cce8f53b8d60b649aca0d6e4a9a05b4f376be
Rafael Alessandro <noprafa@gmail.com>
2018-10-12 11:27:30 -0300

    Documenting communication module
    - Serial Communication class documented

--- Pyfon/communication/serialCommunication.py
+++ Pyfon/communication/serialCommunication.py
@@ -5,26 +5,70 @@ from serial import Serial
 
 class SerialCommunication():
 
-	def __init__(self):
-		self.xbee = None
-		self.serial = None
-		self.robots = Robot().robots
+    """ Serial Communication constructor
+        
+        Initializes xbee and serial as none and receives robots vector from Robot class.
 
-	def startBee(self, port, baud):
-		self.serial = Serial(port, baud)
-		self.xbee = XBee(self.serial)
+        Args:
 
-	def killBee(self):
-		self.serial.close()
+        Returns:
 
-	def sendMessage(self, robotId, message):
-		start_time = time.time()
+    """
+    def __init__(self):
+        self.xbee = None
+        self.serial = None
+        self.robots = Robot().robots
 
-		self.xbee.send("tx", frame = 'A',command='MY',dest_addr=self.robots[robotId-1],data=message)
-		
-		elapsed_time = time.time() - start_time
+    """ Start xbee connection
+        
+        Start xbee connect through serial.
 
-		#print(str(robotId) + " " + str(elapsed_time))
+        Args:
+            port (string): Serial port
+            baud (int): transmission speed
 
-	def newRobot(letter, address):
-		return True
+        Returns:
+
+    """
+    def startBee(self, port, baud):
+        self.serial = Serial(port, baud)
+        self.xbee = XBee(self.serial)
+
+    """ Close xbee connection
+        
+        Kill xbee, closing the serial connection
+
+        Args:
+
+        Returns:
+
+    """
+    def killBee(self):
+        self.serial.close()
+
+    """ Send message
+        
+        Send a message, using xbee method xbee.send(), getting from robots vector the robot address
+        and message received as parameter
+
+        Args:
+            robotId (int): Robot Id
+            message (string): string to send
+
+        Returns:
+
+    """
+    def sendMessage(self, robotId, message):
+        self.xbee.send("tx", frame = 'A',command='MY',dest_addr=self.robots[robotId-1],data=message)
+
+    """ Create a robot
+
+        Args:
+            letter (string): Robot identification
+            address (string): Robot address
+
+        Returns:
+
+    """
+    def newRobot(letter, address):
+        return True
