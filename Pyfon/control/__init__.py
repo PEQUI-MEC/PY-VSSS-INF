e492f207682946102eb6a8128b048c31a3dae271
Luana Martins <luanagbmartins@gmail.com>
2018-10-12 09:20:23 -0300

    Control initial steps (#12)
    * Testando testes
    
    Brincando com testes
    
    * Testes iniciais da comunicação
    
    hehehe
    
    * Adicionando teste do hermes e serial_communication
    
    * Tomando surra da sintaxe do Python
    
    * Testes²
    
    * testes iniciais - ainda não dá pra usar o unittest discover
    
    * testes iniciais
    
    * Delete basic_operations.pyc
    
    * incrementando testes
    
    * Iniciando refatoração: Iniciando implementação do módulo de ações
    
    * Implementação do módulo de ações(forma básica). Faltando algumas modificações
    
    * modificações técnicas no módulo de ações
    
    * modificações ténicas e testes do módulo de ações
    
    * preparando para começar a tradução de comandos
    
    * concertando um calculo no modulo de ações
    
    * controle por orientação feito e controle por posição em progresso
    
    * remoção do __pycache__
    
    * gitignore fixed
    
    * conflictant files fixed
    
    * warrior and endless created, structure organization
    
    * gitignore
    
    * deus me ajuda
    
    * Athena now calculates and returns target commands
    
    * Deleted serial_communication.py
    
    Duplication of files. Kept file serialCommunication.py
    
    * robot de lista para objeto
    
    * limpando algumas coisas comentadas
    
    * getRobots feito, e definindo fluxo geral de zeus
    
    * Mudando acesso aos atributos de Robots em Actions e Translate, método lookAt concertado, método generateOutput feito e tesZeus testando todo fluxo do controle (desde setup a generateOutput). Class Translate incompleta.
    
    * Colocando init para o Zeus
    
    * Usando .self para as chamadas de métodos
    
    * Modificações para testar fluxo geral
    
    * Criando setup no Zeus
    
    * Created constants, roles set, game state and tatics analyzed
    
    * Killed a slave
    
    * Response generator done
    
    * Added avoid obstacles commands
    
    * começando mudanças do getRobots para aprimoramento do actions
    
    * Refatoração do método getRobots. Método 'principal' de Zeus agora chama-se getVelocities
    
    * Iniciando a refatoração do Actions.
    
    * Retirando uma verificação no Translate
    
    * Refatorando o tradutor de mensagens do strategy e começando a comentar os métodos
    
    * Arrumando problemas de verificação e adaptando testActions para as mudanças realizadas
    
    * basic actions almost finished
    
    * Tentando testar um código de uvf que achei na internet. Hades: quando zeus passar por um fluxo não finalizado, as velocidades finais serão [0.0, 0.0]
    
    * stop command refactored and default actions selected
    
    * Fazendo modificações iniciais no unfield, o mesmo ainda apresenta erros que o impedem de ser executado. Mudando nome de robot.py para warrior.py. __Init__.py adicionado no testControl.
    
    * Padronizando e documentando Zeus. Organizando Actions e Translate para passar nos testes mesmo com os métodos incompletos.
    
    * Calculando corretamente theta
    
    * Testando algumas mudanças no uvf, acrescentando atributo 'before' no warrior
    
    * Mudando nome de actions e translate para eunomia e dice respectivamente.
    
    * translate do controle de posição pronto
    
    * Translate pronto para testes.
    
    * mudanças no metodo de navegação

--- Pyfon/control/__init__.py
+++ Pyfon/control/__init__.py
@@ -1,18 +1,1 @@
-from .actions import Actions
-from .translate import Translate
-
-
-class Zeus:
-    robots = []
-
-    def setup(self):
-        return True
-
-    def getRobots(self, robots):
-        pass
-
-    def generateJson(self):
-        pass
-
-    def controlRoutine(self):
-        pass
+from .zeus import Zeus
---
+++ Pyfon/control/zeus.py
@@ -0,0 +1,214 @@
+from .eunomia import Eunomia
+from .dice import Dice
+from .warrior import Warrior
+
+
+class Zeus:
+
+    def __init__(self):
+        self.warriors = []
+        self.nWarriors = 0
+        self.maxVelocity = 1.0
+        self.actions = Eunomia()
+        self.translate = Dice()
+        print("Zeus summoned")
+
+    def setup(self, nWarriors, width=100):
+        """Zeus first movements
+
+        Esse método deverá ser chamado antes de usar Zeus apropriadamente.
+        Aqui é instânciado o Actions e o Translate bem como a quantidade nWarriors de Warriors a serem usados.
+
+        Args:
+            nWarriors (int): Num of warriors in game
+            width (int):
+
+        Returns:
+
+        """
+
+        self.actions.setup(width)
+        self.nWarriors = nWarriors
+
+        for i in range(0, nWarriors):
+            self.warriors.append(Warrior())
+
+        print("Zeus is set up")
+        return self
+
+    def getVelocities(self, strategia):
+        """Zeus main method
+
+        Recebe dados retornados pela estratégia e gera uma lista de Warrior() em getWarriors.
+        Essa lista é encaminhada para o controlRoutine onde serão feitos as chamadas de action.run
+        e translate.run para cada warrior in game e, ao final dessa rotina, é obtido uma lista com
+        as velocidades de cada roda dos warrios que serão enviadas para generateOutput afim de se obter
+        uma lista de dicionários com esses valores.
+
+        Args:
+            strategia (list): Lista de dicionários com as informações geradas pelo Strategy
+
+        Returns:
+            list: Informações a serem passadas para Comunicação(Hermes)
+
+        """
+
+        self.warriors = self.getWarriors(strategia)
+        return self.generateOutput(self.controlRoutine())
+
+    def getWarriors(self, strategia):
+        """Transforma uma lista de dicionários em uma lista de Warrior()
+
+        Seta os atributos do object Warrior() baseado nas informações passadas pela estratégia. Ações que podem ser escolhidas:
+        - {
+            "command": "goTo",
+            "data": {
+                "obstacles": [(x, y)] # opcional - se passado, desviar de tais obstaclos
+                "pose": { "position": (x, y), "orientation": θ radianos },
+                "target": {
+                    "position": (x, y),
+                    "orientation": θ radianos | (x, y)  # opcional - pode ser uma orientação final ou uma posição de lookAt
+                    },
+                "velocity": X m/s,  # opcional - se passado, sem before, é a velocidade constante / com before é velocidade padrão
+                "before": X s  # se passado sem o velocity, usa a velocidade máxima do robô como teto
+            }
+        }
+        - {
+            "command": "spin",
+            "data": { "velocity": X m/s, "direction": "clockwise" | "counter" }
+        }
+        - {
+            "command": "lookAt",
+            "data": {
+                "pose": {
+                    "position": (x, y),  # opcional - é passado se o target for um ponto
+                    "orientation": θ radianos
+                },
+                "target": θ radianos | (x, y)
+            }
+        }
+        - {
+            "command": stop,
+            "data": {before: 0}
+        }
+
+        Args:
+            strategia (list): Lista de dicionários com as informações geradas pelo Strategy
+
+        Returns:
+            list: Lista de object Warrior()
+
+        """
+
+        # TODO(Luana) Testar paralelização com um(1) processo para cada robô.
+        warriors = []
+        if type(strategia) is not list or \
+                len(strategia) != self.nWarriors:
+            raise ValueError("Invalid data object received.")
+
+        for i in range(0, self.nWarriors):
+            if type(strategia[i]) is not dict:
+                raise ValueError("Invalid data object received.")
+
+            if ("command" in strategia[i]) is False or \
+                    ("data" in strategia[i]) is False:
+                raise ValueError("Invalid data object received.")
+
+            warriors.append(Warrior())
+
+        for x in range(0, len(strategia)):
+            if strategia[x]["command"] is not "goTo" and \
+                    strategia[x]["command"] is not "spin" and \
+                    strategia[x]["command"] is not "lookAt" and \
+                    strategia[x]["command"] is not "stop":
+                raise ValueError("Invalid command.")
+
+            warriors[x].action.append(strategia[x]["command"])
+            info = strategia[x]["data"]
+
+            if strategia[x]["command"] == "goTo":
+                warriors[x].position = info["pose"]["position"]
+                warriors[x].orientation = info["pose"]["orientation"]
+
+                warriors[x].target = info["target"]["position"]
+                warriors[x].targetOrientation = info["target"]["orientation"]
+
+                if "velocity" in info:
+                    warriors[x].vMax = info["velocity"]
+                else:
+                    warriors[x].vMax = self.maxVelocity
+
+                if "before" in info:
+                    warriors[x].before = float(info["before"])
+
+                if "obstacles" in info:
+                    warriors[x].obstacles = info["obstacles"]
+
+            elif strategia[x]["command"] == "spin":
+                warriors[x].vMax = info["velocity"]
+                warriors[x].action.append(info["direction"])
+
+            elif strategia[x]["command"] == "lookAt":
+                warriors[x].orientation = info["pose"]["orientation"]
+                if type(info["target"]) is float:
+                    warriors[x].targetOrientation = info["target"]
+                    warriors[x].action.append("orientation")
+                else:
+                    warriors[x].position = info["pose"]["position"]
+                    warriors[x].target = info["target"]
+                    warriors[x].action.append("target")
+
+            elif strategia[x]["command"] == "stop":
+                warriors[x].before = float(info["before"])
+
+        return warriors
+
+    def controlRoutine(self):
+        """Action and Translate call
+
+        Fluxo de cálculos para gerar os pwm's que serão passados para a comunicação
+        Actions() realiza os cálculos baseados nos comandos setados pela estratégia
+        Translate() pega os dados gerados pelo Actions() e calcula a velocidade final de cada roda
+
+        Returns:
+            list: Lista com as velicidades de cada roda dos robôs retornadas pelo Translate.
+
+        """
+
+        # TODO(Luana) Testar paralelização com um(1) processo para cada robô.
+        velocities = []
+        for warrior in self.warriors:
+            if len(warrior.action) > 0:
+                velocities.append(self.translate.run(self.actions.run(warrior)))
+
+        return velocities
+
+    def generateOutput(self, velocities):
+        """Padronização dos dados de saída
+
+        Gera lista de dicionários com as velocidades de cada robô.
+
+        Args:
+            velocities (list): Lista com as velicidades de cada roda dos robôs.
+
+        Returns:
+            list: Lista de dicionário pronto para ser enviado para a Comunicação(Hermes).
+
+        """
+
+        output = [
+            {
+                "vLeft": velocities[0][0],
+                "vRight": velocities[0][1]
+            },
+            {
+                "vLeft": velocities[1][0],
+                "vRight": velocities[1][1]
+            },
+            {
+                "vLeft": velocities[2][0],
+                "vRight": velocities[2][1]
+            }
+        ]
+
+        return output
