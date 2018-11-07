812a4b80160e41c809b8f31091fd2e477d35c75f
Luana Martins <luanagbmartins@gmail.com>
2018-11-07 10:45:49 -0200

    Feature/fixstrategy (#94)
    * Velocidade não estava sendo passada no lookAt e melhorias gerais
    
    * Ângulos de chute calculados pra cada robô
    
    * Adiciona visualização de alvos, corrige consistência na escolha de alvos
    
    Adiciona offset das laterais do goleiro também
    
    * Seleciona atacante baseado no ângulo, spiral faz parte do warrior
    
    Conserta ângulo do alvo na visualização do simulador
    
    * Spiral estava sendo passada incorretamente
    
    * Passando target e orientation como parametro para buildAxis
    
    * Oracle pra predizer posições, reset da athena no play
    
    * Athena trabalha com projeções

--- strategy/warrior.py
+++ strategy/warrior.py
@@ -18,6 +18,8 @@ class Warrior:
 
         self.tactics = "wait"
 
+        self.spiral = 1
+
         # ângulo do robô com a bola
         self.robotBall = 0
         # angulo entre as retas robo->bola e bola->gol
